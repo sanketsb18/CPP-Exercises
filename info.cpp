769d8eb070fe6a36d706132b2a53f5f8c33f89ce
sanketsb18 <sanketsb.18@gmail.com>
2019-11-28 13:29:44 +0530

    c intro
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

---
+++ info.cpp
@@ -0,0 +1,16 @@
+/* Intro to c
+using comments
+printing output
+ending line.*/
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "Hello World!\n";               //Hello world
+    cout << "Name: Sanket Bhosale\n";       //Name
+    cout << "Mobile number:7385393346\n";   //Mobile number
+    cout << "Address:Sr no 53, ""Sai Angan"" bldg, Flatno -7, Netaji chowk, Pimple Gurav " << endl;
+
+    return 0;
+}
