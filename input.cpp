6309e64f1530506e07bbdbb8bbd978d158f3edbd
sanketsb18 <sanketsb.18@gmail.com>
2019-11-28 15:23:33 +0530

    How to take input. uding cin >>
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

---
+++ input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+    int num;
+    cout << "Enter the number:";
+    cin >> num;
+
+    cout << "The entered number is:" << num << endl;
+
+    return 0;
+}
