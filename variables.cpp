4e39f53191acea2fe6766d33c0d07073fc98ae18
sanketsb18 <sanketsb.18@gmail.com>
2019-11-28 14:13:03 +0530

    variables and data types.
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

---
+++ variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int num=56;
+    cout << num << endl;
+    int myNum = 5;               // Integer (whole number without decimals)
+    double myFloatNum = 5.99;    // Floating point number (with decimals)
+    char myLetter = 'D';         // Character
+    string myText = "Hello";     // String (text)
+    bool myBoolean = true;       // Boolean (true or false)
+
+    cout << "I am " << myNum << " Years old" << endl;
+    cout << myFloatNum << endl;
+    cout << myLetter << endl;
+    cout << myText << endl;
+    cout << myBoolean <<endl;
+
+
+    return 0;
+}
