85762da3cf2dea3200005fa0e3357b9493e381f7
sanketsb18 <sanketsb.18@gmail.com>
2019-11-29 17:31:15 +0530

    factorial  using for loop and do while loop.
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,6 @@
 /* C++ Program to find factorial of a number
  Factorial on n = 1*2*3*...*n
-*/
+
 
 #include <iostream>
 using namespace std;
@@ -12,6 +12,26 @@ int main()
     for (i = 1; i <= n; ++i) {
         factorial *= i;   // factorial = factorial * i;
     }
-    cout<< "Factorial of "<<n<<" = "<<factorial  << endl;
+    cout<< "Factorial of "<<n<<" = "<<factorial  << endl;*/
+
+
+
+    /*
+    using do-while loop.
+    */
+    #include<iostream>
+    using namespace std;
+    int main (){
+        int i = 1, n, factorial = 1;
+        cout << "Enter a positive integer: ";
+        cin >> n;
+        while(i<=n){
+            factorial *=i;
+            ++i;
+        }
+            cout<< "factorial of "<<n<<" = "<<factorial << endl;
+    
+
+
     return 0;
 }
