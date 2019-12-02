99884d01e801e2f45f40b5563205b3135dc42595
sanketsb18 <sanketsb.18@gmail.com>
2019-12-02 11:57:14 +0530

    Fibonacci series
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

---
+++ fibonacci.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // 0 1 1 2 3 5 8 ...
+
+    int a = -1, b = 1, c = 0;
+
+    while (c < 100) {
+        c = a + b;
+        a = b;
+        b = c;
+        cout<< c ;
+    }
+    return 0;
+} 
+
